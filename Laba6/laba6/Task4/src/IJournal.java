bf1c596223167d6c53fd48cb64d6383781bef34a
Maxim <fresh88611@gmail.com>
2024-04-26 11:26:23 +0300

    Lab1

--- Laba6/laba6/Task4/src/IJournal.java
+++
@@ -1,9 +0,0 @@
-public interface IJournal {
-    public Book GetBook();
-    public Ticket GetTicket();
-    public String GetDate();
-    public int GetPeriod() ;
-    public String GetReturnDate();
-    public void SetReturnDate(String dateToReturn);
-    public boolean isReturned();
-}
